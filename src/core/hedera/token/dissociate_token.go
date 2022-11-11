21005d0804dd2238b1934aaf126235805d0fa63d
hnamzian <namazian87@gmail.com>
2022-11-11 20:06:44 +0300

    Add function to dissociate token with account

---
+++ src/core/hedera/token/dissociate_token.go
@@ -0,0 +1,42 @@
+package token
+
+import (
+	"fmt"
+
+	"github.com/hashgraph/hedera-sdk-go/v2"
+)
+
+func (t *Token) DissociateWithToken(accountIDString, accountKeyString string) (*hedera.Status, error) {
+	accountID, err := hedera.AccountIDFromString(accountIDString)
+	if err != nil {
+		return nil, fmt.Errorf("invalid accountID")
+	}
+
+	accountKey, err := hedera.PrivateKeyFromString(accountKeyString)
+	if err != nil {
+		return nil, fmt.Errorf("invalid supply key: %s", err)
+	}
+
+	transaction, err := hedera.
+		NewTokenDissociateTransaction().
+		SetTokenIDs(t.TokenID).
+		SetAccountID(accountID).
+		FreezeWith(t.gateway.GetClient())
+	if err != nil {
+		return nil, fmt.Errorf("prepare transaction failed: %s", err)
+	}
+
+	response, err := transaction.
+		Sign(accountKey).
+		Execute(t.gateway.GetClient())
+	if err != nil {
+		return nil, fmt.Errorf("execute transaction failed: %s", err)
+	}
+
+	receipt, err := response.GetReceipt(t.gateway.GetClient())
+	if err != nil {
+		return nil, fmt.Errorf("retreive transaction response failed: %s", err)
+	}
+
+	return &receipt.Status, nil
+}
