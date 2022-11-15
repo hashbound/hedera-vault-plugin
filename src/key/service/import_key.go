487a19f07afba80617dd5016cd0a56597f1c2e2c
hnamzian <namazian87@gmail.com>
2022-11-15 19:12:49 +0300

    Add import key service to import a key into vault storage

---
+++ src/key/service/import_key.go
@@ -0,0 +1,35 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/hashbound/hedera-vault-plugin/src/core/key"
+	"github.com/hashbound/hedera-vault-plugin/src/key/dto"
+	"github.com/hashbound/hedera-vault-plugin/src/key/entity"
+)
+
+func (svc *KeyService) ImportKey(importKey *dto.ImportKeyDTO) (*entity.Key, error) {
+	err := importKey.Validate(); if err != nil {
+		return nil, fmt.Errorf("validate create key failed: %s", err)
+	}
+
+	privateKey := key.PrivateKey{
+		Key: importKey.PrivateKey,
+		Algorithm: key.AlgorithmFromString(importKey.Algorithm),
+	}
+	if importKey.Algorithm == key.Algorithm(key.ECDSA).String() {
+		privateKey.Curve =  key.CurveFromString(importKey.Curve)
+	}
+	
+	keypair, err := key.FromPrivateKey(privateKey)
+	if err != nil {
+		return nil, fmt.Errorf("retreive key pair failed: %s", err)
+	}
+
+	entity := entity.FromKeyPair(importKey.ID, keypair)
+	if err := svc.storage.Write(importKey.ID, entity); err != nil {
+		return nil, fmt.Errorf("write key to storage failed: %s", err)
+	}
+
+	return entity, err
+}
