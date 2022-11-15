6d6f1d3951169b7905f1d2a6eb29b60a9e80be14
hnamzian <namazian87@gmail.com>
2022-11-15 09:36:10 +0300

    Create Curve type

--- src/core/key/create_key_test.go
+++ src/core/key/create_key_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestGenerateKeyPairED25519(t *testing.T) {
-	key, err := GenerateKeyPair(ALGORITHM_ED25519, "")
+	key, err := GenerateKeyPair(ED25519, None)
 	if err != nil {
 		t.Fatalf("Unable to generate key pair %s", err)
 	}
@@ -19,7 +19,7 @@ func TestGenerateKeyPairED25519(t *testing.T) {
 }
 
 func TestGenerateKeyPairECDSA(t *testing.T) {
-	key, err := GenerateKeyPair(ALGORITHM_ECDSA, CURVE_SECP256K1)
+	key, err := GenerateKeyPair(ECDSA, secp256k1)
 	if err != nil {
 		t.Fatalf("Unable to generate key pair %s", err)
 	}
@@ -32,12 +32,12 @@ func TestGenerateKeyPairECDSA(t *testing.T) {
 func TestFromPrivateKeyED25519(t *testing.T) {
 	privateKey := "302e020100300506032b657004220420d937fc715aed14a0433be15c1d7d66cea413837274e32787e717e220d21965fa"
 	publicKey := "302a300506032b657003210027a0ab3727c10325d2ef39ca9f131814b0be4cf2037071b53eb7fd8afd164b3f"
-	algo := ALGORITHM_ED25519
+	algo := ED25519
 
 	key, err := FromPrivateKey(PrivateKey{
 		Key: privateKey,
-		Algorithm: algo,
-		Curve: "",
+		Algorithm: Algorithm(algo),
+		Curve: None,
 	})
 	if err != nil {
 		t.Fatalf("Unable to derive key pair from string private key: %s\n", err)
@@ -51,13 +51,13 @@ func TestFromPrivateKeyED25519(t *testing.T) {
 func TestFromPrivateKeyECDSA(t *testing.T) {
 	privateKey := "3030020100300706052b8104000a04220420c16da8af48c66697f85c8cd05e7886449fd744555b9f688a9e2e6e66cbf8fce3"
 	publicKey := "302f300706052b8104000a032400042102966386144b51b6b22b10044a9e878c1556b565325e0f0dae5492b598d18eba5e"
-	algo := ALGORITHM_ECDSA
-	curve := CURVE_SECP256K1
+	algo := ECDSA
+	curve := secp256k1
 
 	key, err := FromPrivateKey(PrivateKey{
 		Key: privateKey,
-		Algorithm: algo,
-		Curve: curve,
+		Algorithm: Algorithm(algo),
+		Curve: Curve(curve),
 	})
 	if err != nil {
 		t.Fatalf("Unable to derive key pair from string private key: %s\n", err)
