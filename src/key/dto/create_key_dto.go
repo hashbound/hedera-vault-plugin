5003ebf314c974149cbd3cafdb022307dd1132e6
hnamzian <namazian87@gmail.com>
2022-11-15 19:10:50 +0300

    Add create key dto to validate params to create key

---
+++ src/key/dto/create_key_dto.go
@@ -0,0 +1,23 @@
+package dto
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type CreateKeyDTO struct {
+	ID        string `json:"id" validate:"required"`
+	Algorithm string `json:"algorithm" validate:"required,oneof=ED25519 ECDSA"`
+	Curve     string `json:"crve" validate:"required,oneof=secp256k1 ''"`
+}
+
+func (createKeyDTO *CreateKeyDTO) Validate() error {
+	validate := validator.New()
+	err := validate.Struct(createKeyDTO)
+	if err != nil {
+		return fmt.Errorf("validate create key parameters failed: %s", err)
+	}
+
+	return nil
+}
