7392de5e4043c677a1f4f64e2dbbb18f6d66781b
hnamzian <namazian87@gmail.com>
2022-11-15 19:13:39 +0300

    Add delete key service to delete key from vault storage

---
+++ src/key/service/delete_key.go
@@ -0,0 +1,23 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/hashbound/hedera-vault-plugin/src/key/dto"
+)
+
+func (svc *KeyService) Delete(deleteKeyDTO *dto.DeleteKeyDTO) error {
+	err := deleteKeyDTO.Validate(); if err != nil {
+		return fmt.Errorf("validate delete key parameters failed: %s", err)
+	}
+
+	entity, err := svc.storage.Read(deleteKeyDTO.ID)
+	if err != nil {
+		return fmt.Errorf("retreive key before deletion failed: %s", err)
+	}
+	if entity == nil {
+		return fmt.Errorf("key does not exist")
+	}
+	
+	return svc.storage.Delete(deleteKeyDTO.ID)
+}
