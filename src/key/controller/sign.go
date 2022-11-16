e3501bdda7e0212e84af04d4936f545601a78ca4
hnamzian <namazian87@gmail.com>
2022-11-16 11:00:11 +0300

    Add sign message controller to sign a message by key inside vault on request

---
+++ src/key/controller/sign.go
@@ -0,0 +1,39 @@
+package controller
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashbound/hedera-vault-plugin/src/key/dto"
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+)
+
+func Sign(ctx context.Context, req *logical.Request, data *framework.FieldData) (*logical.Response, error) {
+	if req.ClientToken == "" {
+		return nil, fmt.Errorf("client token empty")
+	}
+
+	// Check to make sure that kv pairs provpathed
+	if len(req.Data) == 0 {
+		return nil, fmt.Errorf("data must be provided to store in secret")
+	}
+
+	signMessageDTO := &dto.SignMessageDTO{
+		ID:      data.Get("id").(string),
+		Message: data.Get("message").(string),
+	}
+
+	kc := New(ctx, req)
+	signature, err := kc.service.Sign(signMessageDTO)
+	if err != nil {
+		return nil, err
+	}
+
+	resp := make(map[string]interface{})
+	resp["signature"] = signature
+
+	return &logical.Response{
+		Data: resp,
+	}, nil
+}
