337f6c65edac633afa8e6da7b8458f067d7f537c
hnamzian <namazian87@gmail.com>
2022-11-16 11:28:14 +0300

    Remove required option from curve validator

--- src/key/dto/create_key_dto.go
+++ src/key/dto/create_key_dto.go
@@ -9,7 +9,7 @@ import (
 type CreateKeyDTO struct {
 	ID        string `json:"id" validate:"required"`
 	Algorithm string `json:"algorithm" validate:"required,oneof=ED25519 ECDSA"`
-	Curve     string `json:"crve" validate:"required,oneof=secp256k1 ''"`
+	Curve     string `json:"crve" validate:"oneof=secp256k1 ''"`
 }
 
 func (createKeyDTO *CreateKeyDTO) Validate() error {
