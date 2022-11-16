59b062010d7d537d6ad858581af639a3ed564f63
hnamzian <namazian87@gmail.com>
2022-11-16 11:29:42 +0300

    Remove required oprtion from curve validator in ImportKeyDTO

--- src/key/dto/import_key_dto.go
+++ src/key/dto/import_key_dto.go
@@ -10,7 +10,7 @@ type ImportKeyDTO struct {
 	ID         string `json:"id" validate:"required"`
 	PrivateKey string `json:"privateKey" validate:"required"`
 	Algorithm  string `json:"algorithm" validate:"required,oneof=ED25519 ECDSA"`
-	Curve      string `json:"crve" validate:"required,oneof=secp256k1 ''"`
+	Curve      string `json:"crve" validate:"oneof=secp256k1 ''"`
 }
 
 func (importKeyDTO *ImportKeyDTO) Validate() error {
