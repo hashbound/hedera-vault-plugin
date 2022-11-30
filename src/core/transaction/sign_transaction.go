dac3bdd2c92553b057e6e34be4074d86469c4077
hnamzian <namazian87@gmail.com>
2022-11-30 17:34:23 +0300

    Pas privateKey of type hedera.PrivateKey and remove convert key to hedera.PrivateKey type

--- src/core/transaction/sign_transaction.go
+++ src/core/transaction/sign_transaction.go
@@ -3,22 +3,16 @@ package transaction
 import (
 	"fmt"
 
-	"github.com/hashbound/hedera-vault-plugin/src/core/key"
 	"github.com/hashgraph/hedera-sdk-go/v2"
 )
 
-func SignTransaction(rawTransaction []byte, privateKey *key.PrivateKey) ([]byte, error) {
+func SignTransaction(rawTransaction []byte, privateKey hedera.PrivateKey) ([]byte, error) {
 	tnx, err := hedera.TransactionFromBytes(rawTransaction)
 	if err != nil {
 		return nil, fmt.Errorf("retreive transaction failed: %s", err)
 	}
 
-	keys, err := key.FromPrivateKey(*privateKey)
-	if err != nil {
-		return nil, fmt.Errorf("retreive private key failed: %s", err)
-	}
-
-	signedTransaction, err := hedera.TransactionSign(tnx, keys.PrivateKey)
+	signedTransaction, err := hedera.TransactionSign(tnx, privateKey)
 	if err != nil {
 		return nil, fmt.Errorf("sign transaction failed: %s", err)
 	}
