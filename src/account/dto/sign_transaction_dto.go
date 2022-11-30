ef149b8816b25e11aba55e6d722b71c17c5fedaf
hnamzian <namazian87@gmail.com>
2022-11-30 18:10:39 +0300

    Replace AccountID by ID and remove verifying AccountID in validation

--- src/account/dto/sign_transaction_dto.go
+++ src/account/dto/sign_transaction_dto.go
@@ -4,24 +4,17 @@ import (
 	"fmt"
 
 	"github.com/go-playground/validator/v10"
-	"github.com/hashgraph/hedera-sdk-go/v2"
 )
 
 type SignTransactionDTO struct {
-	AccountID   string `json:"accountId" validate:"required"`
-	Transaction string `json:"transaction" validate:"required"`
+	ID          string `json:"accountId" validate:"required"`
+	Transaction []byte `json:"transaction" validate:"required"`
 }
 
 func (signTransactionDTO *SignTransactionDTO) Validate() error {
 	err := validator.New().Struct(signTransactionDTO)
 	if err != nil {
 		return fmt.Errorf("validate sign transaction parameters failed")
-	}
-
-	// validate AccountID
-	_, err = hedera.AccountIDFromString(signTransactionDTO.AccountID)
-	if err != nil {
-		return fmt.Errorf("invalid account id: %s", err)
 	}
 
 	return nil
