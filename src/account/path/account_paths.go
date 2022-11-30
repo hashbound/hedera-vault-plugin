be0bc5ef09b0dfe625479ca57550a2fcd67421ce
hnamzian <namazian87@gmail.com>
2022-11-30 13:57:01 +0300

    Create account path and expose account controllers

---
+++ src/account/path/account_paths.go
@@ -0,0 +1,92 @@
+package path
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+
+	"github.com/hashbound/hedera-vault-plugin/src/account/controller"
+)
+
+type AccountPaths struct {
+}
+
+func NewKeyPaths() *AccountPaths {
+	return &AccountPaths{}
+}
+
+func (ap *AccountPaths) Paths() []*framework.Path {
+	return framework.PathAppend(
+		[]*framework.Path{
+			ap.pathAccounts(),
+			ap.pathImportAccounts(),
+		},
+	)
+}
+
+func (kp *AccountPaths) pathAccounts() *framework.Path {
+	return &framework.Path{
+		Pattern: "accounts/?",
+
+		Fields: map[string]*framework.FieldSchema{
+			"id": {
+				Type:     framework.TypeString,
+				Required: true,
+			},
+		},
+
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.ReadOperation: &framework.PathOperation{
+				Callback: controller.Get,
+			},
+			logical.ListOperation: &framework.PathOperation{
+				Callback: controller.List,
+			},
+			logical.DeleteOperation: &framework.PathOperation{
+				Callback: controller.Delete,
+			},
+		},
+
+		ExistenceCheck: kp.handleExistenceCheck,
+	}
+}
+
+func (kp *AccountPaths) pathImportAccounts() *framework.Path {
+	return &framework.Path{
+		Pattern: "accounts/import",
+
+		Fields: map[string]*framework.FieldSchema{
+			"id": {
+				Type:     framework.TypeString,
+				Required: true,
+			},
+			"accountId": {
+				Type:     framework.TypeString,
+				Required: true,
+			},
+			"keyId": {
+				Type:     framework.TypeString,
+				Required: true,
+			},
+		},
+
+		Operations: map[logical.Operation]framework.OperationHandler{
+			logical.CreateOperation: &framework.PathOperation{
+				Callback: controller.Import,
+			},
+		},
+
+		ExistenceCheck: kp.handleExistenceCheck,
+	}
+}
+
+func (kp *AccountPaths) handleExistenceCheck(ctx context.Context, req *logical.Request, data *framework.FieldData) (bool, error) {
+	out, err := req.Storage.Get(ctx, req.Path)
+	if err != nil {
+		return false, fmt.Errorf("existence check failed: %s", err)
+	}
+
+	return out != nil, nil
+}
